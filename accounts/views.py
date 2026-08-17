907d0f46847bd093c2c0a214eea6e9bb0d2804d4
Ajay <ajaysunil2001@gmail.com>
2026-08-17 21:23:53 +0530

    revert "fix in logout" is removed from the commit
    This reverts commit 213fc6206b2707371103516b7e6e829530071c45.

--- accounts/views.py
+++ accounts/views.py
@@ -123,8 +123,8 @@ class UserMeView(APIView):
 
 
 class LogoutView(APIView):
-    authentication_classes = []
-    permission_classes = [AllowAny]
+    authentication_classes = [CookieJWTAuthenticationWithoutCSRF]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         refresh_token = request.COOKIES.get("refresh_token")
